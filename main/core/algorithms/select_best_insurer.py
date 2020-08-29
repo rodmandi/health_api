02ec49c655d3bd292b151a86e4d63192edf32d13
rodrigo <rodrigo@theofe.com>
2020-08-29 04:44:55 -0500

    Delete unused libraries

--- main/core/algorithms/select_best_insurer.py
+++ main/core/algorithms/select_best_insurer.py
@@ -1,8 +1,6 @@
 import pandas as pd
 from core.helpers.DefaultLogger import DefaultLogger
-import json
 logger = DefaultLogger.get_logger(__name__)
-
 
 
 def select_best_insurer(insurers_results):
@@ -29,16 +27,13 @@ def select_best_insurer(insurers_results):
 
         if series.values[0] > 1:
             stop_loss = series.values[0]
-
         oop_max = df["oop_max"].min()
         if (deductible is None) & (stop_loss is None):
-            df = df[(df["oop_max"] == oop_max)].sort_values(by=["deductible", "stop_loss"]).iloc[0, :]
-        if (deductible is None):
-            df = df[(df["oop_max"] == oop_max) &
-                        (df["stop_loss"] == stop_loss)].sort_values(by=["deductible"]).iloc[0, :]
-        if (stop_loss is None):
-            df = df[(df["oop_max"] == oop_max) &
-                        (df["deductible"] == deductible)].sort_values(by=["stop_loss"]).iloc[0, :]
+            df = df[df["oop_max"] == oop_max].sort_values(by=["deductible", "stop_loss"]).iloc[0, :]
+        if deductible is None:
+            df = df[df["stop_loss"] == stop_loss].sort_values(by=["deductible", "oop_max"]).iloc[0, :]
+        if stop_loss is None:
+            df = df[df["deductible"] == deductible].sort_values(by=["stop_loss", "oop_max"]).iloc[0, :]
 
     result = df.iloc[0, :].to_dict()
     logger.info(result)
